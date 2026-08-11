10c3c274bf241b4bad2fc2bb0bc7978cce3bbda3
Alireza Mohammadi <alireza1384928@gmail.com>
2026-08-11 20:47:27 +0330

    Factor out forms to a snippet

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,14 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django import forms
 
 
+class LoginForm(AuthenticationForm):
+    template_name = "accounts/form_snippet.html"
+
+
 class RegisterForm(UserCreationForm):
+    template_name = "accounts/form_snippet.html"
     email = forms.EmailField(required=False)
 
     class Meta:
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
-from . import views
+from . import views, forms
 
 app_name = "accounts"
 
 urlpatterns = [
     path("register/", views.register_view, name="register"),
-    path("login/", LoginView.as_view(template_name="accounts/login.html"), name="login"),
+    path("login/", LoginView.as_view(
+        template_name="accounts/login.html", authentication_form=forms.LoginForm
+        ), name="login"),
     path("logout/", views.logout_view, name="logout"),
 ]
