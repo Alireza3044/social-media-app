fa2e89f3f78d2c62c3de53df19c424276fd7e491
Alireza Mohammadi <alireza1384928@gmail.com>
2026-08-16 15:49:51 +0330

    Add accounts:password-change view, form and template

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,16 +1,20 @@
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import forms as auth_forms
 from django.contrib.auth.models import User
 from django import forms
 
 
-class LoginForm(AuthenticationForm):
-    template_name = "accounts/form_snippet.html"
-
-
-class RegisterForm(UserCreationForm):
+class RegisterForm(auth_forms.UserCreationForm):
     template_name = "accounts/form_snippet.html"
     email = forms.EmailField(required=False)
 
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+
+class LoginForm(auth_forms.AuthenticationForm):
+    template_name = "accounts/form_snippet.html"
+
+
+class PasswordChangeForm(auth_forms.PasswordChangeForm):
+    template_name="accounts/form_snippet.html"
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView
 from . import views, forms
 
 app_name = "accounts"
@@ -8,7 +8,16 @@ urlpatterns = [
     path("", views.index_view, name="index"),
     path("register/", views.register_view, name="register"),
     path("login/", LoginView.as_view(
-        template_name="accounts/login.html", authentication_form=forms.LoginForm
+        template_name="accounts/login.html",
+        form_class=forms.LoginForm
         ), name="login"),
     path("logout/", views.logout_view, name="logout"),
+    path("password-change/", PasswordChangeView.as_view(
+        template_name="accounts/password_change.html",
+        form_class=forms.PasswordChangeForm
+        ), name="password-change"),
+    path("password-change/done/", PasswordChangeView.as_view(
+        template_name="accounts/password_change.html",
+        form_class=forms.PasswordChangeForm
+        ), name="password-change"),
 ]
