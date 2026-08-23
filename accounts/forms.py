21d8c097774a3c824b370c1b0479389c25588aa7
Alireza Mohammadi <alireza1384928@gmail.com>
2026-08-23 16:14:31 +0330

    Add user edit and profile edit forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import forms as auth_forms
 from django.contrib.auth.models import User
 from django import forms
+from .models import Profile
 
 
 class RegisterForm(auth_forms.UserCreationForm):
@@ -10,6 +11,21 @@ class RegisterForm(auth_forms.UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "first_name", "last_name", "email", "password1", "password2"]
+
+
+class UserEditForm(forms.ModelForm):
+    template_name = "accounts/form_snippet.html"
+    email = forms.EmailField(required=False)
+
+    class Meta:
+        model = User
+        fields = ["username", "first_name", "last_name", "email"]
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["image"]
 
 
 class LoginForm(auth_forms.AuthenticationForm):
