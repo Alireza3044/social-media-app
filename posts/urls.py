12daff2cd77cb21c0d8785b904e0ec344e19ab6f
Alireza Mohammadi <alireza1384928@gmail.com>
2026-08-24 16:47:46 +0330

    Add post create page

--- posts/urls.py
+++ posts/urls.py
@@ -5,4 +5,5 @@ app_name = "posts"
 
 urlpatterns = [
     path("", views.PostListView.as_view(), name="post-list"),
+    path("create/", views.PostCreateView.as_view(), name="post-create"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,22 @@
-from django.views.generic import ListView
-from . import models
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
+from . import models, forms
 
 
 class PostListView(ListView):
     model = models.Post
+    template_name = "posts/post_list.html"
     context_object_name = "posts"
+
+
+class PostCreateView(CreateView):
+    form_class = forms.PostForm
+    template_name = "posts/post_create.html"
+    success_url = reverse_lazy("posts:post-list")
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+
+        return super().form_valid(form)
