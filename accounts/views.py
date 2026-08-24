8646c0c5205ec7734b0f16b16fa0de78158b4ee6
Alireza Mohammadi <alireza1384928@gmail.com>
2026-08-24 12:00:28 +0330

    Simplify profile_edit_view

--- accounts/views.py
+++ accounts/views.py
@@ -11,29 +11,25 @@ class ProfileView(TemplateView):
 
 
 def profile_edit_view(request):
+    user_form = forms.UserEditForm(instance=request.user)
+    profile_form = forms.ProfileEditForm(instance=request.user.profile)
+    
     if request.method == "POST":
-        user_form = forms.UserEditForm(data=request.POST, instance=request.user)
-        profile_form = forms.ProfileEditForm(
-            data=request.POST,
-            files=request.FILES,
-            instance=request.user.profile
-        )
+        user_form = forms.UserEditForm(request.POST, instance=request.user)
+        profile_form = forms.ProfileEditForm(request.POST, request.FILES, instance=request.user.profile)
         
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
             return redirect("accounts:profile")
     
-    if request.method == "GET":
-        user_form = forms.UserEditForm(instance=request.user)
-        profile_form = forms.ProfileEditForm(instance=request.user.profile)
-        context = {
-            "user_form": user_form,
-            "profile_form": profile_form,
-            "header": "Edit Profile",
-            "btn_text": "Submit",
-        }
-        return render(request, "accounts/profile_edit.html", context)
+    context = {
+        "user_form": user_form,
+        "profile_form": profile_form,
+        "header": "Edit Profile",
+        "btn_text": "Submit",
+    }
+    return render(request, "accounts/profile_edit.html", context)
 
 
 def register_view(request):
