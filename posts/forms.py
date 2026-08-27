8a724972c513dcb6baa5c2df5112f576b2086435
Alireza Mohammadi <alireza1384928@gmail.com>
2026-08-27 18:24:23 +0330

    Set a default form snippet for PostForm

--- posts/forms.py
+++ posts/forms.py
@@ -3,6 +3,8 @@ from .models import Post
 
 
 class PostForm(forms.ModelForm):
+    template_name = "posts/form_post_snippet.html"
+
     class Meta:
         model = Post
         fields = ["title", "image", "caption"]
