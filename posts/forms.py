4f9379a2489a4309952af789e06f0e335ec8fcc7
Alireza Mohammadi <alireza1384928@gmail.com>
2026-08-30 18:00:56 +0330

    Refactor comment input attributes to a Django form

--- posts/forms.py
+++ posts/forms.py
@@ -14,3 +14,12 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["body"]
+        widgets = {
+            "body": forms.Textarea({
+                "class": "w-full h-24 bg-gray-100 border-2 border-gray-400 rounded-lg px-2 py-1 resize-none",
+                "placeholder": "Write a comment..."
+            })
+        }
+        labels = {
+            "body": ""
+        }
