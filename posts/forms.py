b1c06d5d15364d21f6c128b1f781a98886f20f5e
Alireza Mohammadi <alireza1384928@gmail.com>
2026-09-01 12:47:13 +0330

    Refactor & improve the post create form

--- posts/forms.py
+++ posts/forms.py
@@ -4,11 +4,30 @@ from .models import Post, Comment
 
 
 class PostForm(forms.ModelForm):
-    template_name = "accounts/form_snippet.html"
-
     class Meta:
         model = Post
         fields = ["title", "image", "caption"]
+        widgets = {
+            "title": forms.TextInput({
+                "class": "px-2 py-1 w-full bg-gray-100 border-2 border-gray-400 rounded-lg shadow focus:outline-blue-500",
+                "placeholder": "Title...",
+            }),
+            "image": forms.FileInput({
+                "id": "image-input",
+                "class": "hidden",
+                "accept": "image/*",
+                "x-ref": "imageInput",
+                "@change": "fileChosen($event)",
+            }),
+            "caption": forms.Textarea({
+                "class": "px-2 py-1 w-full h-30 bg-gray-100 border-2 border-gray-400 rounded-lg resize-none shadow focus:outline-blue-500",
+                "placeholder": "Caption...",
+            })
+        }
+        labels = {
+            "title": "",
+            "caption": "",
+        }
 
 
 class CommentForm(forms.ModelForm):
